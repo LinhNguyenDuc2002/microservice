5c4b9f37b6076e2f925d111a2c08b9d1e96edb5c
LinhNguyen <linhlinhlinh16042002@gmail.com>
2025-05-25 23:12:10 +0700

    create apigateway

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableCaching
 @EnableConfigurationProperties({KeycloakSpringBootProperties.class}) // automatically enable configuration
 @ComponentScan(basePackages = {"com.example.productservice", "com.example.servicefoundation"})
